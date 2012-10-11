a65733e8f589ad5aa5951a64848dd892c0d1fac0
Levi Wilson <levi@leviwilson.com>
2012-10-10 23:32:09 -0400

    api level 11 minimum

--- app/src/com/leandog/puppies/PuppiesActivity.java
+++ app/src/com/leandog/puppies/PuppiesActivity.java
@@ -20,7 +20,6 @@ public class PuppiesActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_puppies, menu);
         return true;
     }
-
     
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
