a0a3dccc1db473d36aba140c98ccb07501391d74
Levi Wilson <levi@leviwilson.com>
2012-10-25 16:01:18 -0400

    include empty PuppyTaleActivity

---
+++ app/src/com/leandog/puppies/PuppyTaleActivity.java
@@ -0,0 +1,37 @@
+package com.leandog.puppies;
+
+import com.leandog.puppies.R.layout;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class PuppyTaleActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_puppy_tale);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_puppy_tale, menu);
+        return true;
+    }
+
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
