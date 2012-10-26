476f7d5fdac45d5751076ed1e27485ef0b8c5312
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-10-26 16:26:19 -0400

    fixed maggie

--- cukes/features/support/screens/puppy_list.rb
+++ cukes/features/support/screens/puppy_list.rb
@@ -3,7 +3,7 @@ class PuppyList
 
   list_item(:sparky, :text => 'Sparky')
   list_item(:ruby_sue, :text => 'Ruby Sue')
-  list_item(:magggie_mae, :text => 'Maggie Mae')
+  list_item(:maggie_mae, :text => 'Maggie Mae')
 
   def view_for(name)
     self.send "#{name.downcase.gsub(' ', '_')}_view"
