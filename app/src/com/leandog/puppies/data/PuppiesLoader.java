a0ec03cf737f45d2165b5e479b044ffea6e8a578
Levi Wilson <levi@leviwilson.com>
2012-10-27 23:26:25 -0400

    implement PuppiesLoader

--- app/src/com/leandog/puppies/data/PuppiesLoader.java
+++ app/src/com/leandog/puppies/data/PuppiesLoader.java
@@ -1,11 +1,35 @@
 package com.leandog.puppies.data;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import com.google.gson.Gson;
 
 public class PuppiesLoader {
 
     public List<Puppy> load() {
-        return null;
+        try {
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(new URL("http://puppies.herokuapp.com/puppies.json").openStream()));
+            
+            final StringBuilder content = new StringBuilder();
+            String line;
+            
+            while(null != (line = reader.readLine())) {
+                content.append(line);
+            }
+            
+            return Arrays.asList(new Gson().fromJson(content.toString(), Puppy[].class));
+        } catch (MalformedURLException e) {
+        } catch (IOException e) {
+        }
+        
+        return new ArrayList<Puppy>();
     }
 
 }
