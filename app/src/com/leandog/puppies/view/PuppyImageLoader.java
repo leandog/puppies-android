714c681a6c2a9286ca48ba94ee4ca6130d2ea0ad
Levi Wilson <levi@leviwilson.com>
2012-10-28 00:25:27 -0400

    implement PuppyImageLoader; headshot scenario passing

--- app/src/com/leandog/puppies/view/PuppyImageLoader.java
+++ app/src/com/leandog/puppies/view/PuppyImageLoader.java
@@ -1,10 +1,19 @@
 package com.leandog.puppies.view;
 
+import com.google.android.imageloader.ImageLoader;
+
 import android.widget.ImageView;
 
 public class PuppyImageLoader {
+    
+    private ImageLoader imageLoader;
+
+    public PuppyImageLoader() {
+        imageLoader = new ImageLoader();
+    }
 
     public void bind(final ImageView imageView, final String imageUrl) {
+        imageLoader.bind(imageView, imageUrl, null);
     }
 
 }
