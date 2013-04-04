fd7d6655184d8ee339d64fa2c78caf23a5a1336a
David Shah <david.a.shah@gmail.com>
2013-04-04 10:45:05 -0400

    Point 0

--- app/src/com/leandog/puppies/PuppiesActivity.java
+++ app/src/com/leandog/puppies/PuppiesActivity.java
@@ -1,92 +1,17 @@
 package com.leandog.puppies;
 
-import static com.leandog.puppies.view.ViewHelper.findFor;
-import static com.leandog.puppies.view.ViewHelper.hide;
-
-import java.util.List;
-
 import android.app.ActionBar;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListView;
 
-import com.leandog.puppies.R.id;
 import com.leandog.puppies.R.layout;
-import com.leandog.puppies.data.PuppiesLoader;
-import com.leandog.puppies.data.Puppy;
-import com.leandog.puppies.view.PuppyImageLoader;
 
 public class PuppiesActivity extends Activity {
-
-    private final PuppiesLoader puppiesLoader;
-    private final PuppyImageLoader puppyImageLoader;
-
-    public PuppiesActivity() {
-        this(new PuppiesLoader(), new PuppyImageLoader());
-    }
-
-    public PuppiesActivity(PuppiesLoader puppiesLoader,PuppyImageLoader puppyImageLoader) {
-        this.puppiesLoader = puppiesLoader;
-        this.puppyImageLoader = puppyImageLoader;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_puppies);
-
-        initializeActionBar();
-
-        ListView thePuppies = findFor(this, id.the_puppies_list);
-        thePuppies.setOnItemClickListener(new OnDisplayThePuppyTale(thePuppies));
-        new AsyncPuppiesLoader(thePuppies).execute();
-    }
-
-    private void initializeActionBar() {
         getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getActionBar().setCustomView(layout.happy_banner);
     }
-
-    private final class OnDisplayThePuppyTale implements OnItemClickListener {
-    	
-        private final ListView thePuppies;
-
-        private OnDisplayThePuppyTale(ListView thePuppies) {
-            this.thePuppies = thePuppies;
-        }
-
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            final Intent theDetails = new Intent(PuppiesActivity.this, PuppyTaleActivity.class);
-            theDetails.putExtra("thePuppy", thePuppyAt(position).toJson());
-            startActivity(theDetails);
-        }
-
-        private Puppy thePuppyAt(int position) {
-            return (Puppy) thePuppies.getItemAtPosition(position);
-        }
-    }
-
-    private final class AsyncPuppiesLoader extends AsyncTask<Void, Void, List<Puppy>> {
-        private final ListView thePuppies;
-
-        private AsyncPuppiesLoader(ListView thePuppies) {
-            this.thePuppies = thePuppies;
-        }
-
-        @Override
-        protected List<Puppy> doInBackground(Void... params) {
-            return puppiesLoader.load();
-        }
-
-        protected void onPostExecute(List<Puppy> puppies) {
-            thePuppies.setAdapter(new PuppyAdapter(puppyImageLoader, PuppiesActivity.this, puppies));
-            hide(PuppiesActivity.this, id.loading);
-        }
-    }
-
 }
--- app/src/com/leandog/puppies/PuppyTaleActivity.java
+++ app/src/com/leandog/puppies/PuppyTaleActivity.java
@@ -1,47 +1,16 @@
 package com.leandog.puppies;
 
-import static com.leandog.puppies.view.ViewHelper.findFor;
-import static com.leandog.puppies.view.ViewHelper.setText;
 import android.os.Bundle;
-import android.text.Html;
-import android.widget.ImageView;
 
-import com.leandog.puppies.R.id;
 import com.leandog.puppies.R.layout;
-import com.leandog.puppies.data.Puppy;
-import com.leandog.puppies.view.PuppyImageLoader;
 
 public class PuppyTaleActivity extends Activity {
-
-    private final PuppyImageLoader puppyImageLoader;
-    
-    public PuppyTaleActivity() {
-        this(new PuppyImageLoader());
-    }
-
-    public PuppyTaleActivity(PuppyImageLoader puppyImageLoader) {
-        this.puppyImageLoader = puppyImageLoader;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_puppy_tale);
         getActionBar().setDisplayHomeAsUpEnabled(true);
-        
-        Puppy thePuppy = getPuppy();
-        setText(this, id.name, thePuppy.getName());
-        setText(this, id.breed, thePuppy.getBreed());
-        setText(this, id.gender, thePuppy.getGender());
-        setText(this, id.description, Html.fromHtml(thePuppy.getDescription()));
-        setText(this, id.adoption_fee, String.format("$%.2f", thePuppy.getFee()));
-        
-        ImageView theHeadshot = findFor(this, id.headshot);
-        puppyImageLoader.bind(theHeadshot, thePuppy.getImageUrl());
-    }
-
-    private Puppy getPuppy() {
-        return Puppy.fromJson(getIntent().getStringExtra("thePuppy"));
     }
 
 }
