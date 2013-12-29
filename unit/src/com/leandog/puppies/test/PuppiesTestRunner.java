327ec1b468b8a35595a491728067fef11bf0a59f
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2013-12-29 08:28:50 -0500

    updated to work with latest robolectric

--- unit/src/com/leandog/puppies/test/PuppiesTestRunner.java
+++ unit/src/com/leandog/puppies/test/PuppiesTestRunner.java
@@ -1,26 +1,28 @@
 package com.leandog.puppies.test;
 
-import static com.xtremelabs.robolectric.Robolectric.bindShadowClass;
-
 import java.io.File;
 import java.lang.reflect.Method;
 
 import org.junit.runners.model.InitializationError;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import com.leandog.puppies.shadows.ShadowActionBarActivity;
-import com.xtremelabs.robolectric.RobolectricTestRunner;
 
+@Config(manifest = "../app/AndroidManifest.xml")
 public class PuppiesTestRunner extends RobolectricTestRunner {
 
     public PuppiesTestRunner(Class<?> testClass) throws InitializationError {
-        super(testClass, new File("../app"));
+        super(testClass);
     }
+    
+    
 
-    @Override
-    public void beforeTest(Method method) {
-        bindShadowClass(ShadowActionBarActivity.class);
-    }
+//    @Override
+//    public void beforeTest(Method method) {
+//        bindShadowClass(ShadowActionBarActivity.class);
+//    }
 
     @Override
     public Object createTest() throws Exception {
