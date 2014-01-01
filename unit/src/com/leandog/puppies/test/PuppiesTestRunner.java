26d7f270183a2b066e0852d851b6caf2286abb41
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2014-01-01 14:27:54 -0500

    moved back to separate projects

--- unit/src/com/leandog/puppies/test/PuppiesTestRunner.java
+++ unit/src/com/leandog/puppies/test/PuppiesTestRunner.java
@@ -1,29 +1,15 @@
 package com.leandog.puppies.test;
 
-import java.io.File;
-import java.lang.reflect.Method;
-
 import org.junit.runners.model.InitializationError;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 
-import com.leandog.puppies.shadows.ShadowActionBarActivity;
-
-@Config(manifest = "../app/AndroidManifest.xml")
 public class PuppiesTestRunner extends RobolectricTestRunner {
 
     public PuppiesTestRunner(Class<?> testClass) throws InitializationError {
         super(testClass);
     }
     
-    
-
-//    @Override
-//    public void beforeTest(Method method) {
-//        bindShadowClass(ShadowActionBarActivity.class);
-//    }
-
     @Override
     public Object createTest() throws Exception {
         final Object theTest = super.createTest();
