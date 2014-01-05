7fef88e750d590e9c541698fa3edb029f30e2393
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2014-01-05 12:24:54 -0500

    moved initMocks into test class

--- app/test/com/leandog/puppies/PuppiesActivityTest.java
+++ app/test/com/leandog/puppies/PuppiesActivityTest.java
@@ -1,23 +1,22 @@
 package com.leandog.puppies;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.verify;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.util.ActivityController;
 
 import com.leandog.puppies.data.PuppyLoader;
 import com.leandog.puppies.shadows.ShadowActionBarActivity;
-import com.leandog.puppies.test.PuppiesTestRunner;
 
 @Config(shadows = ShadowActionBarActivity.class)
-@RunWith(PuppiesTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 public class PuppiesActivityTest {
 
 	@Mock private PuppyLoader puppiesLoader;
@@ -26,6 +25,7 @@ public class PuppiesActivityTest {
 
 	@Before
 	public void setup() {
+        MockitoAnnotations.initMocks(this);
 		controller = Robolectric.buildActivity(PuppiesActivity.class);
 		activity = controller.get();		
 		activity.setPuppyLoader(puppiesLoader);		
